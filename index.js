4fe0f18e409417e4e734575bbc4be5e52a214bcc
Aliasgar Murtuza <aliasgar@getpostman.com>
2020-04-14 23:50:05 +0530

    Add ability to send whatsapp

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ let Mafia = require('./lib/mafia'),
     {
       name: 'Hawwa Mohammed',
       mobile: '971566577618',
-      isPlaying: false
+      isPlaying: true
     },
     {
       name: 'Murtaza Laxmi',
